da6b975b68e9afadc1e80a330d2036363e79084b
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-29 13:25:57 +0300

    webpack reconfigured

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+'use strict';
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  watch: true,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {test: /\.js?$/, exclude: /node_modules/, loaders: ['babel']}
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: './src/index.html', to: '.' }])
+  ]
+};
