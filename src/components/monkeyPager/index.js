68089377a519c68d19928e1f61b46ce1c4299ea5
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-07-03 15:26:42 +0300

    dating profile upgrade

--- src/components/monkeyPager/index.js
+++ src/components/monkeyPager/index.js
@@ -7,7 +7,7 @@ import profile_dating_com from './profile_dating_com'
 export default class MonkeyPager {
   constructor() {
     switch (window.location.host) {
-      case "www.dating.com": profile_dating_com();
+      case "www.dating.com": new profile_dating_com();
     }
     console.log(`monkey is ready to keep track of your page ${monkey}`);
   }
--- src/components/monkeyPager/profile_dating_com.js
+++ src/components/monkeyPager/profile_dating_com.js
@@ -1,40 +1,61 @@
 'use strict';
-import price from './../../price'
+import Pricer from "../pricer/";
 
-function _findSuspend() {
-  const alarmElems = document.querySelectorAll('.admin-panel .alarm');
 
-  for (let i = 0; i < alarmElems.length; i++) {
-    if (alarmElems[i].childNodes[5].innerText == 'Suspend') {
-      return alarmElems[i];
-    }
+export default class profile_dating_com {
+  constructor() {
+    this.pricer = new Pricer();
+    this.buttons = {};
+    this.profile = {
+      id: window.location.hash.slice(1),
+      scam: false,
+      suspend: false,
+      date: null,
+      price: 0
+    };
+
+    const buttons = document.querySelectorAll('.control.button.toggle');
+    buttons.forEach(item => {
+      switch (item.childNodes[5].innerText) {
+        case 'Suspend':
+        {
+          this.buttons.suspend = item;
+          this.buttons.suspend.addEventListener('mouseover', ()=> this._onSuspend());
+          break;
+        }
+
+        case 'Scam':
+        {
+          this.buttons.scam = item;
+
+          break;
+        }
+
+        case 'Approve':
+        {
+          this.buttons.approve = item;
+          this.buttons.approve.addEventListener('mouseover', ()=> this._onApprove());
+          break;
+        }
+      }
+    });
   }
-}
 
-function _onApprove() {
-  const photoEmpty = '/i/account/fake-avatar-big.png?2';
-  const photo = document.querySelector('thumbnail photo');
-
-  let result = {
-    id: '',
-    hasPhoto: false
-  };
-
-  if (photo.src !== photoEmpty) {
-    result.hasPhoto = true;
+  _onSuspend() {
+    this.profile.date = Date.now();
+    this.profile.scam = !!this.buttons.scam.classList.contains('pressed');
+    this.profile.suspend = true;
+    if (this.profile.scam) alert('Упс, это как то странно, суспендить скамера');
+    this.profile.price = this.pricer.calculate(this.profile);
+    console.log(`suspend ${JSON.stringify(this.profile)}`);
   }
-  console.log(`approve`)
-}
 
-function _onSuspend() {
-  console.log(`suspend`)
-}
+  _onApprove() {
 
-export default function profile_dating_com() {
-  const profileId = window.location.hash.slice(1);
-  const approveBtn = document.querySelector('.admin-panel .approve');
-  const suspendBtn = _findSuspend();
-
-  approveBtn.addEventListener('mouseover', () => _onApprove());
-  suspendBtn.addEventListener('mouseover', () => _onSuspend());
+    this.profile.date = Date.now();
+    this.profile.scam = !!this.buttons.scam.classList.contains('pressed');
+    // if (this.buttons.approve.classList.contains('pressed')) console.log('')
+    this.profile.price = this.pricer.calculate(this.profile);
+    console.log(`approve ${JSON.stringify(this.profile)}`);
+  }
 }
