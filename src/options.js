df1a76a0b8824546b9a01c9f0e9f3b1c6db47957
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-07-04 23:15:22 +0300

    adding confirm

--- src/options.js
+++ src/options.js
@@ -3,4 +3,4 @@ import Db from "./components/db";
 
 const monkey = new Db();
 
-document.querySelector('.reset-db').addEventListener('click', ()=> monkey.reset());
+document.querySelector('.reset-db').addEventListener('click', ()=> confirm('You sure?') ? monkey.reset() : null);
