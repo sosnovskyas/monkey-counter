307074ac47a36592425b7b0dc18914ad8d5ba9d6
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-07-04 18:08:48 +0300

    upgrade popup

---
+++ src/components/popupLog/index.js
@@ -0,0 +1,25 @@
+'use strict';
+import Db from "./../db";
+import template from './popupLog.jade'
+
+export default class PopupLog {
+  constructor({elem}) {
+    const monkey = new Db({name: 'monkey'});
+
+    monkey.select(10, result => {
+      let rows = [];
+      for(let i = 0; i < result.length; i++){
+        console.log(result[i]);
+        rows.push({
+          date: new Date(result[i].timestamp),
+          project: result[i].label
+        })
+      }
+      
+      elem.innerHTML = template({
+        rows: rows
+      });
+      
+    })
+  }
+}
