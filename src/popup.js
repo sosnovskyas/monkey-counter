e8db3a0a2bb13d26bdc100efcebf42634650a95b
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-07-04 17:33:12 +0300

    DB integrated to popup

--- src/popup.js
+++ src/popup.js
@@ -1,1 +1,13 @@
 'use strict';
+import Db from "./components/db";
+
+const monkey = new Db({name: 'monkey'});
+
+const log = document.querySelector('.log');
+
+log.innerHTML = 'test';
+
+
+monkey.select(10, result => {
+  log.innerHTML = JSON.stringify(result);
+})
